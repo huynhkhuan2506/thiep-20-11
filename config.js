ba01212cbe3875ecc7fa6f4530b2d46d975fc530
Huỳnh Khuân <113177630+huynhkhuan2506@users.noreply.github.com>
2022-11-10 23:42:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Cô Cẩm Loan';
+const giftUrl = '';
+const eventName = 'Chào mừng 20/11';
+const titleCard = 'Tặng Cô Cẩm Loan';
+const contentCard = 'Chúc cô luôn tràn đầy sức khoẻ, luôn nhiệt huyết trong những năm, tháng cầm phấn viết bảng, chúc cô luôn tìm được đam mê và tiếp bước cho thế hệ sau!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
